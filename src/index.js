747d06d965c2e0a0bc87ec44ac8c7ab67c098d80
MuhdHanish <muhammedhanish11@gmail.com>
2023-05-18 12:55:37 +0530

    context created and its provider wraped the App

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import {FirebaseContext} from './store/firebaseContext' 
+import db from './firebase/config';
 
 ReactDOM.render(
  <React.StrictMode>
+  <FirebaseContext.Provider value={db}>
   <Router>
    <App />
   </Router>
+  </FirebaseContext.Provider>
  </React.StrictMode>
  , document.getElementById('root'));
