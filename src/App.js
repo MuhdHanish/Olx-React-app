55c196b66577d1110e7236717ecdbba337d7f7cf
MuhdHanish <muhammedhanish11@gmail.com>
2023-05-18 22:50:29 +0530

    create form completed add useState to all feild and image upload also added

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Home from './Pages/Home';
 import Signup from './Pages/Signup';
 import Login from './Pages/Login';
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import Create from './Pages/Create';
 
 function App() {
   const {setUser} = useContext(AuthContext);
@@ -25,6 +26,7 @@ function App() {
         <Route path='/' exact element={<Home/>}/>
         <Route path='/signup' element={<Signup/>}/>
         <Route path='/login' element={<Login/>}/>
+        <Route path='/create' element={<Create/>}/>
       </Routes>
     </div>
   );
--- src/Components/Create/Create.js
+++ src/Components/Create/Create.js
@@ -1,8 +1,18 @@
-import React, { Fragment } from 'react';
+import React, { Fragment ,useState} from 'react';
 import './Create.css';
 import Header from '../Header/Header';
+import {HandelState} from '../../useForm'
 
 const Create = () => {
+
+  const [state,setState] = HandelState({
+    name:'',
+    category:'',
+    price:''
+  })
+
+  const[image,setImage] = useState(null)
+  console.log(state)
   return (
     <Fragment>
       <Header />
@@ -15,7 +25,9 @@ const Create = () => {
               className="input"
               type="text"
               id="fname"
-              name="Name"
+              name="name"
+              value={state.name}
+              onChange={setState}
               defaultValue="John"
             />
             <br />
@@ -25,20 +37,25 @@ const Create = () => {
               className="input"
               type="text"
               id="fname"
+              value={state.category}
+              onChange={setState}
               name="category"
               defaultValue="John"
             />
             <br />
             <label htmlFor="fname">Price</label>
             <br />
-            <input className="input" type="number" id="fname" name="Price" />
+            <input className="input" type="number" value={state.price}
+              onChange={setState} id="fname" name="price" />
             <br />
           </form>
           <br />
-          <img alt="Posts" width="200px" height="200px" src=""></img>
+          <img  alt="Posts" width="200px" height="200px" src={image?URL.createObjectURL(image):''}></img>
           <form>
             <br />
-            <input type="file" />
+            <input onChange={(event)=>{
+             setImage(event.target.files[0])
+            }} type="file" />
             <br />
             <button className="uploadBtn">upload and Submit</button>
           </form>
